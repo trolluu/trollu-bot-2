c572e039e747953dab139637738f59f6776db368
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 03:51:56 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,10 @@
 const { RichEmbed } = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+    const embed = new RichEmbed()
+    .setDescription("Help menu")
+    .setColor("RANDOM")
+    .addField("Commands", "help, play, pause, resume, skip, leave, queue, volume");
 
-const embed = new RichEmbed()
-.setDescription("Help menu")
-.setColor("RANDOM")
-.addField("Commands", "help, play, pause, resume, skip, leave, queue, volume");
-
-message.channel.send(embed);
+    message.channel.send(embed);
+}
