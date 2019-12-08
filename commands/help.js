4cc91cb54154c76b197a57692de7cfe222a48637
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 04:13:30 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     const embed = new RichEmbed()
     .setDescription("Help menu")
     .setColor("RANDOM")
-    .addField("Commands:", "( help, play, pause, resume, skip, leave, queue, volume )").then(m => m.delete(10000));
+    .addField("Commands:", "( help, play, pause, resume, skip, leave, queue, volume )");
 
     message.channel.send(embed);
 }
