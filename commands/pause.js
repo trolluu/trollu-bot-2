24d4ac18e73e01692dcfee7837b9d495fec3ee87
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 02:28:01 +0100

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args, ops) => {
     let fetched = ops.active.get(message.guild.id);
 
     if(!fetched) return message.channel.send("There currently isn\'t any music playing in this guild!");
-    if(message.member,voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you aren\'t in the same channel as the music bot.");
+    if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you aren\'t in the same channel as the music bot.");
     if(fetched.dispatcher.paused) return message.channel.send("This music is alread paused.");
 
     fetched.dispatcher.pause();
