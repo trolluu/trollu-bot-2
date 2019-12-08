04ea496ab2a352555dac14e88d1ab87e7102ee96
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 04:11:32 +0100

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -1,8 +1,8 @@
 exports.run = (client, message, args, ops) => {
     let fetched = ops.active.get(message.guild.id);
 
-    if(!fetched) return message.channel.send("There currently isn's any music playing in this guild!");
-    if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, ypu currently aren\'t in the same channel as the bot.");
+    if(!fetched) return message.channel.send("There currently isn's any music playing in this guild!").then(m => m.delete(5000));
+    if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, ypu currently aren\'t in the same channel as the bot.").then(m => m.delete(5000));
     if(isNaN(args[0]) || args[0 > 200 || args[0] < 0]) return message.channel.send("Please input a number between 0-200");
 
     fetched.dispatcher.setVolume(args[0]/100);
