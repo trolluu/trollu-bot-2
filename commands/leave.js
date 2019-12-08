0cd571f70d5cc96b6b819325df61be2d6a27f98d
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 04:11:19 +0100

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,8 +1,8 @@
 exports.run = (client, message, args, ops) => {
-    if(!message.member.voiceChannel) return message.channel.send("Please connect to a voice channel.");
-    if(!message.guild.me.voiceChannel) return message.channel.send("Sorry, bot isn\'t connected to the guild.");
-    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Sorry, you aren\'t connected to the same channel.");
+    if(!message.member.voiceChannel) return message.channel.send("Please connect to a voice channel.").then(m => m.delete(5000));
+    if(!message.guild.me.voiceChannel) return message.channel.send("Sorry, bot isn\'t connected to the guild.").then(m => m.delete(5000));
+    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Sorry, you aren\'t connected to the same channel.").then(m => m.delete(5000));
 
     message.guild.me.voiceChannel.leave();
-    message.channel.send("Leaving Channel...");
+    message.channel.send("Leaving Channel...").then(m => m.delete(5000));
 }
