c26043fd68480d3c1bcf20a61783268f4022b5f3
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 03:18:39 +0100

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,8 +4,8 @@ exports.run = async(client, message, args, ops) => {
     if(!fetched) return message.channel.send("There currently isn\'t any music playing in the guild!");
     if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you currently aren\'t in the same channel as the bot!");
 
-    let userCount = message.member.voiceChannel.members.size;
-    let required = Math.ceil(userCount/2);
+    //let userCount = message.member.voiceChannel.members.size;
+    //let required = Math.ceil(userCount/2);
 
     if(!fetched.queue[0].voteSkips) fetched.queue[0].voteSkips = [];
     if(fetched.queue[0].voteSkips.includes(message.member.id)) return message.channel.send(`Sorry, you already voted to skip! ${fetched.queue[0].voteSkips.length}/${required} required.`);
